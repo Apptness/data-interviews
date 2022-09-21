1f2b9109c8991b1e5560555e6f5268b231157fe1
Raghu <raghu@apptness.io>
2022-09-21 14:23:34 -0400

    Update import.sh

--- docker-entrypoint-initdb.d/import.sh
+++ docker-entrypoint-initdb.d/import.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-psql --username "apptness" --dbname "apptness" -c "\copy sms_sends from ../../../docker-entrypoint-initdb.d/data/sms_sends.csv delimiter ',' csv header;"
-psql --username "apptness" --dbname "apptness" -c "\copy sms_provider_response from ../../../docker-entrypoint-initdb.d/data/sms_provider_response.csv delimiter ',' csv header;"
-psql --username "apptness" --dbname "apptness" -c "\copy tiny_url_clicks from ../../../docker-entrypoint-initdb.d/data/tiny_url_clicks.csv delimiter ',' csv header;"
-psql --username "apptness" --dbname "apptness" -c "\copy tiny_url_search from ../../../docker-entrypoint-initdb.d/data/tiny_url_search.csv delimiter ',' csv header;"
-psql --username "apptness" --dbname "apptness" -c "\copy tiny_urls from ../../../docker-entrypoint-initdb.d/data/tiny_urls.csv delimiter ',' csv header;"
+psql --username "apptness" --dbname "apptness" -c "\copy cfe from ../../../docker-entrypoint-initdb.d/data/cfe.csv delimiter ',' csv header;"
+psql --username "apptness" --dbname "apptness" -c "\copy flows from ../../../docker-entrypoint-initdb.d/data/flows.csv delimiter ',' csv header;"
+psql --username "apptness" --dbname "apptness" -c "\copy publishers from ../../../docker-entrypoint-initdb.d/data/publishers.csv delimiter ',' csv header;"
